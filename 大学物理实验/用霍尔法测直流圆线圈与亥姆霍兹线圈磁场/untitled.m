dbca2410982e2eabd3250595e3c084499a82f0f8
zjuatri <zjuatri@qq.com>
2024-11-11 17:10:30 +0800

    update

---
+++ 大学物理实验/用霍尔法测直流圆线圈与亥姆霍兹线圈磁场/untitled.m
@@ -0,0 +1,14 @@
+mu = 4*pi*10^(-7);
+N=400;
+I=0.4;
+R=0.1;
+x = linspace(-5,5,100);
+b = (mu*N*I*(R^2)/2)./((x./100).^2 + R^2).^(3/2) * 10^6;
+X=[5.00	4.00	3.00	2.00	1.00	0.00	-1.00	-2.00	-3.00	-4.00	-5.00];
+B=[703	786	862	925	972	981	957	914	845	773	691];
+plot(X,B,'ro',x,b,'b-');
+xlabel('X/cm',FontName="Times New Roman",FontAngle="italic");
+ylabel('B/\mu T',FontName="Times New Roman",FontAngle="italic");
+title('载流圆线圈轴线上磁场的分布','FontName','黑体');
+x = 5;
+B = (mu*N*I*(R^2)/2)/((x./100)^2 + R^2)^(3/2) * 10^6
