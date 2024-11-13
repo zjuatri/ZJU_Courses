9cbcb938474ce416974312fb106269ecbf68ee80
zjuatri <zjuatri@qq.com>
2024-11-13 15:14:42 +0800

    update

--- 大学物理实验/用霍尔法测直流圆线圈与亥姆霍兹线圈磁场/untitled2.m
+++ 大学物理实验/用霍尔法测直流圆线圈与亥姆霍兹线圈磁场/untitled2.m
@@ -2,6 +2,6 @@
 X=[5.00	4.00	3.00	2.00	1.00	0.00	-1.00	-2.00	-3.00	-4.00	-5.00];
 B=[1222	1122	1061	1017	990	986	995	1024	1064	1128	1232];
 plot(X,B,'bo-');
-xlabel('X/cm',FontName="Times New Roman",FontAngle="italic");
+xlabel('Y/cm',FontName="Times New Roman",FontAngle="italic");
 ylabel('B/\mu T',FontName="Times New Roman",FontAngle="italic");
 title('载流圆线圈轴线上中心平面径向磁场的分布','FontName','黑体');
